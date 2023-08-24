2a4c2b95e9f730d17e43cc7a0452da42a0e0d162
Ravi Samani <ravi.samani@netcracker.com>
2023-08-24 17:23:55 +0530

    Add CandidateForm.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template>
   <div id="App">
-    <Candidate />
     <Login />
+    <Candidate/>
+    <CandidateForm/>
     <Demands />
   </div>
 </template>
 
 <script>
-import Candidate from './components/Candidate.vue'
 import Login from './components/Login.vue'
+import Candidate from './components/Candidate.vue'
+import CandidateForm from './components/CandidateForm.vue'
 import Demands from './components/Demands.vue'
-
 
 export default {
   name: 'App',
   components: {
     Login,
     Candidate,
-    Demands
+    CandidateForm,
+    Demands,
   }
 }
 </script>
---
+++ src/components/CandidateForm.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="app" class="candidate-form-container">
+        <h1 class="text-center">Please Enter Available Candidate Details.</h1>
+        <table class="table table-striped">
+            <thead>
+                <th>Candidate ID</th>
+                <th>Candidate First Name</th>
+                <th>Candidate Last Name</th>
+                <th>Candidate Email ID</th>
+            </thead>
+            <tbody>
+                <tr v-for="candidate in candidates" v-bind:key="candidate.id">
+                    <td>{{candidate.id}}</td>
+                    <td>{{candidate.firstName}}</td>
+                    <td>{{candidate.lastName}}</td>
+                    <td>{{candidate.emailId}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script type="module">
+import CandidateService from '../services/CandidateService'
+    export default {
+        name: 'CandidateForm-component',
+        data(){
+            return {
+                candidates : []
+            }
+            
+        },
+        methods: {
+            getCandidates(){
+                CandidateService.getCandidates().then((response) => {
+                    this.candidates = response.data;
+                }
+                );
+            }
+        },
+        created() {
+            this.getCandidates()
+        }
+    }
+</script>
+
