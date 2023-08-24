4fe7a1e725912bd16ad4d59c2819970669bc351d
Rohan Jagtap <rohan.jagtap@netcracker.com>
2023-08-24 12:11:02 +0530

    test

--- src/components/Candidate.vue
+++ src/components/Candidate.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <h1 class="text-center">Available Candidates List.</h1>
+        <h1 class="text-center">Available Candidates List</h1>
         <table class="table table-striped">
             <thead>
                 <th>Candidate ID</th>
