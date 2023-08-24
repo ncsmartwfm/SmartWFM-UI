ea2798b1ad9b456b1269069efe2379bb3baa0f88
Rohan Jagtap <rohan.jagtap@netcracker.com>
2023-08-24 22:30:17 +0530

    header and footer added

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,15 @@
+<template>
+    <footer>
+      <p>Netcracker Copyright &copy; "current year" </p>
+    </footer>
+  </template>
+
+<script>
+import { computed } from 'vue'
+
+export default {
+  setup () {
+    const year = computed(() => new Date().getFullYear())
+  }
+}
+</script>
