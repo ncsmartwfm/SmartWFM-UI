1daa3ef742a33723f4b5256371fdebdf41130a02
Ravi Samani <ravi.samani@netcracker.com>
2023-08-26 17:49:39 +0530

    LM Screen - Layout

--- src/components/CandidateForm.vue
+++ src/components/CandidateForm.vue
@@ -1,64 +1,66 @@
 <template>
-  <div class="vue-tempalte">
-    <h3>Add Candidate</h3>
-    <form @submit.prevent="submitForm">
-      <div class="form-group">
-        <label>Candidate ID</label>
-        <input type="text" class="form-control form-control-lg" v-model="formData.candidateId" required/>
-      </div>
-      <div class="form-group">
-        <label>First Name</label>
-        <input type="text" class="form-control form-control-lg" v-model="formData.firstName" required/>
-      </div>
-      <div class="form-group">
-        <label>Last Name</label>
-        <input type="text" class="form-control form-control-lg" v-model="formData.lastName" required/>
-      </div>
-      <div class="form-group">
-        <label>Domain</label>
-        <input type="text" class="form-control form-control-lg" v-model="formData.domain" required/>
-      </div>
-      <div class="form-group">
-        <label>Skill Set (Comma Separated)</label>
-        <input type="text" class="form-control form-control-lg" v-model="formData.skillSet" required/>
-      </div>
+  <div class="vertical-center">
+    <div class="inner-block">
+      <div class="vue-tempalte">
+        <h3>Add Candidate</h3>
+        <form @submit.prevent="submitForm">
+          <div class="form-group">
+            <label>Candidate ID</label>
+            <input type="text" class="form-control form-control-lg" v-model="formData.candidateId" required/>
+          </div>
+          <div class="form-group">
+            <label>First Name</label>
+            <input type="text" class="form-control form-control-lg" v-model="formData.firstName" required/>
+          </div>
+          <div class="form-group">
+            <label>Last Name</label>
+            <input type="text" class="form-control form-control-lg" v-model="formData.lastName" required/>
+          </div>
+          <div class="form-group">
+            <label>Domain</label>
+            <input type="text" class="form-control form-control-lg" v-model="formData.domain" required/>
+          </div>
+          <div class="form-group">
+            <label>Skill Set (Comma Separated)</label>
+            <input type="text" class="form-control form-control-lg" v-model="formData.skillSet" required/>
+          </div>
 
-      <div class="form-group">
-        <label>Years of Experience</label>
-        <input type="number" class="form-control form-control-lg" v-model="formData.yearsOfExperience" required/>
+          <div class="form-group">
+            <label>Years of Experience</label>
+            <input type="number" class="form-control form-control-lg" v-model="formData.yearsOfExperience" required/>
+          </div>
+          <div>
+            <label>Years of Experience in current domain</label>
+            <input type="number" class="form-control form-control-lg" v-model="formData.yearOfExperienceInCurrentDomain" required/>
+          </div>
+          <div>
+            <label>Email</label>
+            <input type="email" class="form-control form-control-lg" v-model="formData.candidateEmailId" required/>
+          </div>
+          <div>
+            <label>LM ID</label>
+            <input type="text" class="form-control form-control-lg" v-model="formData.lineManagerOfficialId" required/>
+          </div>
+          <div>
+            <label>LM Name</label>
+            <input type="text" class="form-control form-control-lg" v-model="formData.lineManagerName" required/>
+          </div>
+          <div>
+            <label>LM Email</label>
+            <input type="email" class="form-control form-control-lg" v-model="formData.lineManagerEmailId" required/>
+          </div>
+          <div>
+            <label>Country</label>
+            <input type="text" class="form-control form-control-lg" v-model="formData.country" required/>
+          </div>
+          <div>
+            <label>City</label>
+            <input type="text" class="form-control form-control-lg" v-model="formData.city" required/>
+          </div>
+          <button class="btn btn-outline-primary" type="submit">Submit</button>
+        </form>
       </div>
-      <div>
-        <label>Years of Experience in current domain</label>
-        <input type="number" class="form-control form-control-lg" v-model="formData.yearOfExperienceInCurrentDomain" required/>
-      </div>
-      <div>
-        <label>Email</label>
-        <input type="email" class="form-control form-control-lg" v-model="formData.candidateEmailId" required/>
-      </div>
-      <div>
-        <label>LM ID</label>
-        <input type="text" class="form-control form-control-lg" v-model="formData.lineManagerOfficialId" required/>
-      </div>
-      <div>
-        <label>LM Name</label>
-        <input type="text" class="form-control form-control-lg" v-model="formData.lineManagerName" required/>
-      </div>
-      <div>
-        <label>LM Email</label>
-        <input type="email" class="form-control form-control-lg" v-model="formData.lineManagerEmailId" required/>
-      </div>
-      <div>
-        <label>Country</label>
-        <input type="text" class="form-control form-control-lg" v-model="formData.country" required/>
-      </div>
-      <div>
-        <label>City</label>
-        <input type="text" class="form-control form-control-lg" v-model="formData.city" required/>
-      </div>
-      <button type="submit">
-Add<!--        <router-link class="btn btn-outline-primary" to="/line-manager">Add</router-link>-->
-      </button>
-    </form>
+    </div>
   </div>
 </template>
 
@@ -91,7 +93,7 @@ export default {
   methods: {
     async submitForm() {
       console.log('Form submitted with data:', JSON.stringify(this.formData));
-      this.formData.skillSet=this.formData.skillSet.split(',');
+      this.formData.skillSet = this.formData.skillSet.split(',');
       console.log('Form submitted with data:', JSON.stringify(this.formData));
       const response = await axios.post('http://10.230.24.183:8080/candidates', this.formData);
       console.log('Response:', response.data);
--- src/components/DemandForm.vue
+++ src/components/DemandForm.vue
@@ -1,43 +1,46 @@
 <template>
-  <div class="vue-tempalte">
-    <h3>Add Demand</h3>
-    <form @submit.prevent="submitForm">
-      <div class="form-group">
-        <label>Demand ID</label>
-        <input type="text" class="form-control form-control-lg" v-model="formData.demandId" required/>
+  <div class="vertical-center">
+    <div class="inner-block">
+      <div class="vue-tempalte">
+        <h3>Add Demand</h3>
+        <form @submit.prevent="submitForm">
+          <div class="form-group">
+            <label>Demand ID</label>
+            <input type="text" class="form-control form-control-lg" v-model="formData.demandId" required/>
+          </div>
+          <div class="form-group">
+            <label>Project Name</label>
+            <input type="text" class="form-control form-control-lg" v-model="formData.projectName" required/>
+          </div>
+          <div class="form-group">
+            <label>Project Role</label>
+            <input type="text" class="form-control form-control-lg" v-model="formData.projectRole" required/>
+          </div>
+          <div class="form-group">
+            <label>desiredSkillSet</label>
+            <input type="text" class="form-control form-control-lg" v-model="formData.desiredSkillSet" required/>
+          </div>
+          <div class="form-group">
+            <label>DesiredYearsOfExperience</label>
+            <input type="text" class="form-control form-control-lg" v-model="formData.desiredYearsOfExperience" required/>
+          </div>
+          <div class="form-group">
+            <label>Domain</label>
+            <input type="text" class="form-control form-control-lg" v-model="formData.domain" required/>
+          </div>
+          <div class="form-group">
+            <label>Location</label>
+            <input type="text" class="form-control form-control-lg" v-model="formData.location" required/>
+          </div>
+          <div class="form-group">
+            <label>Desired Location</label>
+            <input type="text" class="form-control form-control-lg" v-model="formData.desiredLocation" required/>
+          </div>
+          <button class="btn btn-outline-primary" type="submit">Submit
+          </button>
+        </form>
       </div>
-      <div class="form-group">
-        <label>Project Name</label>
-        <input type="text" class="form-control form-control-lg" v-model="formData.projectName" required/>
-      </div>
-      <div class="form-group">
-        <label>Project Role</label>
-        <input type="text" class="form-control form-control-lg" v-model="formData.projectRole" required/>
-      </div>
-      <div class="form-group">
-        <label>desiredSkillSet</label>
-        <input type="text" class="form-control form-control-lg" v-model="formData.desiredSkillSet" required/>
-      </div>
-      <div class="form-group">
-        <label>DesiredYearsOfExperience</label>
-        <input type="text" class="form-control form-control-lg" v-model="formData.desiredYearsOfExperience" required/>
-      </div>
-      <div class="form-group">
-        <label>Domain</label>
-        <input type="text" class="form-control form-control-lg" v-model="formData.domain" required/>
-      </div>
-      <div class="form-group">
-        <label>Location</label>
-        <input type="text" class="form-control form-control-lg" v-model="formData.location" required/>
-      </div><div class="form-group">
-        <label>Desired Location</label>
-        <input type="text" class="form-control form-control-lg" v-model="formData.desiredLocation" required/>
-      </div>
-
-      <button type="submit">
-        Add<!--        <router-link class="btn btn-outline-primary" to="/line-manager">Add</router-link>-->
-      </button>
-    </form>
+    </div>
   </div>
 </template>
 
